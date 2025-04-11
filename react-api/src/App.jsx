64bb6b9c4b90415353fc67bbecd6261ebe74f9a7
BassaniDavid <davbassan98@gmail.com>
2025-04-11 15:10:59 +0200

    feat: milestone 1

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [actress, SetActress] = useState([])
+  function fetchActress() {
+    //https://jsonplaceholder.typicode.com/posts
+    axios.get("https://www.freetestapi.com/api/v1/actresses")
+      .then((res) => console.log(res.data))
+  }
+
+  useEffect(fetchActress, [])
+
 
   return (
     <>
+
 
     </>
   )
